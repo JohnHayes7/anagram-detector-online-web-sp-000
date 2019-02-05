2e1ec440d29ff404099857a9060be183aad93114
John Hayes <johnnyhayes7@gmail.com>
2019-02-05 19:44:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
+require "pry"
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  @@word_array = []
+  
+  def initialize(word)
+    @word = word
+    @@word_array << @word
+  end
+  
+  def match(word_array)
+    match_array = []
+    @@word_array.collect do |i|
+     if i.split("").sort == @word.split("").sort
+       match_array << i
+     end
+    end
+    match_array
+  end
+  
+  
+end
